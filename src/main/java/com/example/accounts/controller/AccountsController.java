ad08628596b7e7f847e2799607753e1cfafc9ba4
Simon Vargas <sjvargas01@gmail.com>
2023-09-06 15:13:12 -0400

    refactored files

--- src/main/java/com/example/accounts/controller/AccountsController.java
+++ src/main/java/com/example/accounts/controller/AccountsController.java
@@ -4,23 +4,21 @@ import com.example.accounts.model.Accounts;
 import com.example.accounts.model.Customer;
 import com.example.accounts.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
-    @PostMapping("/myAccount")
-    public Accounts getAccountDetails(@RequestBody Customer customer) {
-        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-        if(accounts != null) {
-            return accounts;
-        } else {
-            return null;
-        }
+    @GetMapping("/myAccounts")
+    List<Accounts> accountsList() {
+            return (List<Accounts>) accountsRepository.findAll();
     }
 
 }
--- src/main/java/com/example/accounts/model/Customer.java
+++ src/main/java/com/example/accounts/model/Customer.java
@@ -24,44 +24,4 @@ public class Customer {
         private String mobileNumber;
         @Column(name = "create_dt")
         private LocalDate createDt;
-
-        public int getCustomerId() {
-                return customerId;
-        }
-
-        public void setCustomerId(int customerId) {
-                this.customerId = customerId;
-        }
-
-        public String getName() {
-                return name;
-        }
-
-        public void setName(String name) {
-                this.name = name;
-        }
-
-        public String getEmail() {
-                return email;
-        }
-
-        public void setEmail(String email) {
-                this.email = email;
-        }
-
-        public String getMobileNumber() {
-                return mobileNumber;
-        }
-
-        public void setMobileNumber(String mobileNumber) {
-                this.mobileNumber = mobileNumber;
-        }
-
-        public LocalDate getCreateDt() {
-                return createDt;
-        }
-
-        public void setCreateDt(LocalDate createDt) {
-                this.createDt = createDt;
-        }
 }
